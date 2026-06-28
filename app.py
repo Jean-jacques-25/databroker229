6ffd5d910854a4bd15634d0296432b59dac8147a
Jean-jacques-25 <jeanjacquesaguin30@gmail.com>
2026-06-28 19:26:50 +0100

    Ajout vérification Google Search Console

---
+++ app.py
@@ -0,0 +1,9 @@
+
+@app.route('/googleb745c890fd4fea44.html')
+def google_verify():
+    return 'google-site-verification: b745c890fd4fea44'
+EOFcat >> ~/databroker229/app.py << 'EOF'
+
+@app.route('/googleb745c890fd4fea44.html')
+def google_verify():
+    return 'google-site-verification: b745c890fd4fea44'
