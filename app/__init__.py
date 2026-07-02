abec277ee04c04b8ecdc11399117106ecdd72152
Jean-jacques-25 <jeanjacquesaguin30@gmail.com>
2026-07-02 22:11:14 +0100

    Add Flask-Mail config

--- app/__init__.py
+++ app/__init__.py
@@ -1,10 +1,12 @@
 from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
 from flask_migrate import Migrate
+from flask_mail import Mail
 import os
 
 db = SQLAlchemy()
 migrate = Migrate()
+mail = Mail()
 
 def create_app():
     app = Flask(__name__)
@@ -20,8 +22,17 @@ def create_app():
     app.config['WTF_CSRF_ENABLED'] = False
     app.config['MAX_CONTENT_LENGTH'] = 16 * 1024 * 1024
 
+    # Config Email
+    app.config['MAIL_SERVER']   = 'smtp.gmail.com'
+    app.config['MAIL_PORT']     = 587
+    app.config['MAIL_USE_TLS']  = True
+    app.config['MAIL_USERNAME'] = 'databroker229@gmail.com'
+    app.config['MAIL_PASSWORD'] = os.environ.get('MAIL_PASSWORD', '')
+    app.config['MAIL_DEFAULT_SENDER'] = ('LaCentraleDesDonnées229', 'databroker229@gmail.com')
+
     db.init_app(app)
     migrate.init_app(app, db)
+    mail.init_app(app)
 
     from app.routes import main
     from app.admin_routes import admin
